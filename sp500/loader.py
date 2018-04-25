ee123f50a52d1d4c1dda9250c7ceaaf0ea40b08e
liangxun <liangxun21@gmail.com>
2018-04-25 12:22:32 +0800

    完善了在多特征输入情况下的数据预处理

--- sp500/loader.py
+++ sp500/loader.py
@@ -56,30 +56,32 @@ class DataPreprocess(object):
             x_train.shape, y_train.shape, x_test.shape, y_test.shape))
         return [x_train, y_train, x_test, y_test]
 
-    def lstm_load_multidata(self, filename,seq_len,dim=5,normalise_bool=True,row=2000):
+    def lstm_load_multidata(self, filename, seq_len, dim=2, normalise_bool=True, row=2000):
         """LSTM多特征输入"""
-        self.mode = 'lstm_mul'
+        self.mode = 'lstm_dim{}'.format(dim)
         self.dim = dim
         table = pd.read_csv(filename,index_col='Date')
-        data = np.log(table)
+        #data = np.log(table)
+        data = table
         if normalise_bool is True:
             self.max = data.max()
             self.min = data.min()
+            self.col_close = data.columns.get_loc('Close')
             data = self.normalise(data)
         data = np.array(data)
         sequence_length = seq_len + 1
         result = []
         for index in range(len(data) - sequence_length):
             result.append(data[index: index + sequence_length])
-        print("相空间重构后数据集的长度：",len(result))
+        print("相空间重构后数据集的长度：", len(result))
         result = np.array(result)
         train = result[:row, :]
         print("train set的长度：", row)
-        #np.random.shuffle(train)
+        #输入多维特征，但输出依旧是只有close一维
         x_train = train[:, :-1]
-        y_train = train[:, -1, -2]
+        y_train = train[:, -1, self.col_close]
         x_test = result[row:, :-1]
-        y_test = result[row:, -1, -2]
+        y_test = result[row:, -1, self.col_close]
         print("test set的长度：", len(y_test))
         x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], dim))
         x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], dim))
@@ -125,21 +127,25 @@ class DataPreprocess(object):
         return [x_train, y_train, x_test, y_test]
 
     def normalise(self, data):
+        """将所以列的数据都归一化到（0,1）"""
         normalised_data = (data-self.min)/(self.max-self.min)
         return normalised_data
 
-    def unnormalise(self, data, index=-2):
-        """在lstm的输入维度大于2时，需要制定max中那一列是close对应的最小值"""
+    def unnormalise(self, data):
+        """
+        只需要恢复close这一列。
+        但lstm的输入维度大于2时，max保存了所有列的最大值，需要指定Close所在列的下标"""
         if self.dim is 1:
             recovered_data = data * (self.max-self.min) + self.min
         else:
-            recovered_data = data*(self.max[index]-self.min[index])+self.min[index]
+            recovered_data = data*(self.max[self.col_close] - self.min[self.col_close]) + self.min[self.col_close]
         return recovered_data
 
     def recover(self, data):
         if self.normalise_bool is True:
             data = self.unnormalise(data)
-        recovered_data = np.exp(data)
+        # recovered_data = np.exp(data)
+        recovered_data = data
         return recovered_data
 
 
@@ -151,12 +157,13 @@ def show(data,label='data'):
 
 
 if __name__ == '__main__':
-    datapath = '../dataset/sp2005-2015.csv'
+    dim = 4
+    datapath = '../dataset/sp2009_dim{}.csv'.format(dim)
     print('> Loading data... ')
     DataLoader = DataPreprocess()
     #x_train, y_train, x_test, y_test = DataLoader.lstm_load_data(datapath, 50)
     #data, (train,test) = DataLoader.arima_load_data(datapath,50)
-    x_train, y_train, x_test, y_test = DataLoader.svm_load_data(datapath, 50)
+    x_train, y_train, x_test, y_test = DataLoader.lstm_load_multidata(datapath,50,dim)
     show(y_train)
     show(y_test)
     show(DataLoader.recover(y_test))
