09edb2804b17f4e4906e076215495023e321ba2f
liangxun <liangxun21@gmail.com>
2018-04-25 15:09:23 +0800

    添加多特征输入下的lstm模型定义和训练部分代码

--- sp500/lstm_model.py
+++ sp500/lstm_model.py
@@ -9,7 +9,8 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # Hide messy TensorFlow warnings
 warnings.filterwarnings("ignore")   # Hide messy Numpy warnings
 
 
-def build_model(input_shape, layers_out, lr=0.001, dropout=0.1):
+def lstm_2(input_shape, layers_out, lr=0.001, dropout=0.1):
+    model_name = 'lstm2'
     model = Sequential()
     model.add(LSTM(
         input_shape=input_shape,
@@ -25,12 +26,12 @@ def build_model(input_shape, layers_out, lr=0.001, dropout=0.1):
     model.add(Dense(
         output_dim=layers_out[2],
         activation='linear'))
-
     model.compile(loss="mse", optimizer=optimizers.rmsprop(lr=lr))
-    return model
+    return model, model_name
 
 
-def single_model(input_shape, layers_out, lr=0.001, dropout=0.1):
+def lstm_1(input_shape, layers_out, lr=0.001, dropout=0.1):
+    model_name = 'lstm_1'
     model = Sequential()
     model.add(LSTM(
         input_shape=input_shape,
@@ -45,20 +46,24 @@ def single_model(input_shape, layers_out, lr=0.001, dropout=0.1):
     ))
 
     model.compile(loss='mse', optimizer=optimizers.rmsprop(lr=lr))
-    return model
+    return model, model_name
+
 
 def lstm_bp(input_shape,layers_out,lr=0.001,dropout=0.01):
+    model_name = 'lstm_bp'
     model = Sequential()
-    model.add(LSTM(layers_out[0],input_shape=input_shape, dropout=dropout,return_sequences=False))
-    #model.add(LSTM(layers_out[1],dropout=dropout,return_sequences=False))
-    model.add(Dense(layers_out[1], activation='tanh'))
-    model.add(Dense(layers_out[2]))
-    model.compile(optimizer=optimizers.rmsprop(lr=lr),loss='mse')
-    return model
+    model.add(LSTM(layers_out[0], input_shape=input_shape, dropout=dropout,return_sequences=True))
+    model.add(LSTM(layers_out[1], dropout=dropout, return_sequences=False))
+    model.add(Dense(layers_out[2], activation='relu'))
+    model.add(Dense(layers_out[3]))
+    model.compile(optimizer=optimizers.rmsprop(lr=lr), loss='mse')
+    return model, model_name
+
 
 if __name__ == '__main__':
-    layer_out = [10,20,30,1]
-    input_shape = (12,7)
-    model = lstm_bp(input_shape,layer_out)
+    layer_out = [32, 64, 32, 1]
+    input_shape = (12, 7)
+    model,model_name= lstm_bp(input_shape, layer_out)
     print(model.summary())
-    #plot_model(model,to_file='./model.png',show_shapes=True,show_layer_names=False)
+    #plot_model(model, show_shapes=True, show_layer_names=False)
+    plot_model(model, to_file='./model.png', show_shapes=True, show_layer_names=False)
